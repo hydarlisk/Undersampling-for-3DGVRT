ee64112e8e8cc167b4a9260873fdb77880f3aee0
KJS <goliath917@gmail.com>
2026-03-27 20:00:59 +0900

    Debug: change window log format

--- base/Logger.h
+++ base/Logger.h
@@ -14,9 +14,9 @@
 #define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG,   LOG_TAG, __VA_ARGS__))
 #define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR,   LOG_TAG, __VA_ARGS__))
 #else
-#define LOGV(...) ((void)(printf("[V][%s] ", "vulkan"), printf(__VA_ARGS__), printf("\n")))
-#define LOGI(...) ((void)(printf("[I][%s] ", "vulkan"), printf(__VA_ARGS__), printf("\n")))
-#define LOGW(...) ((void)(printf("[W][%s] ", "vulkan"), printf(__VA_ARGS__), printf("\n")))
-#define LOGD(...) ((void)(printf("[D][%s] ", "vulkan"), printf(__VA_ARGS__), printf("\n")))
+#define LOGV(...) ((void)(printf(__VA_ARGS__), printf("\n")))
+#define LOGI(...) ((void)(printf(__VA_ARGS__), printf("\n")))
+#define LOGW(...) ((void)(printf(__VA_ARGS__), printf("\n")))
+#define LOGD(...) ((void)(printf(__VA_ARGS__), printf("\n")))
 #define LOGE(...) ((void)(fprintf(stderr, "[E][%s] ", "vulkan"), fprintf(stderr, __VA_ARGS__), fprintf(stderr, "\n")))
 #endif
